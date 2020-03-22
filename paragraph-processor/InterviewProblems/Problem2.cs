a2aabe977e96eefbff6439fff7f8bfc47004ab63
MADDY\daeer <daeer73@gmail.com>
2020-03-22 15:42:23 +0000

    Added the last solution.

---
+++ paragraph-processor/InterviewProblems/Problem2.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewProblems
+{
+    /// <summary>
+    /// The function should take a string, and return a dictionary that returns a count of the number of occurrences of
+    /// each word in the string.
+    ///
+    /// This function has been partially completed, can you finish it? - Yes.
+    ///
+    /// If you were writing this function from scratch, how would you do it, what would you do differently and why?
+    ///
+    /// What I would do:
+    /// 1. It is part of a utility and I would make a utility
+    /// solution / project.
+    ///
+    /// 2. I would extract it to an interface so that it could be mocked using moq, nsubstitute or fake it easy.
+    ///
+    /// 3. I would then have the build server create the solution / project as a NuGet package.
+    ///
+    /// 4. Using the existing test I have created, I would then start to think how I would change this method.
+    ///
+    /// 5. The code didn't work, as the dictionary needed the words inserted first.  As the phrase is being split into
+    /// a string array, I didn't need the extra dictionary.  Then used a concurrent dictionary to add or update using
+    /// the word passed in.  If the word existed then the count would be increased.
+    ///
+    /// 6. The replacing of the characters could be made better, but I'm not sure how you could replace any non-letter
+    /// character.  Unless you had another utility method that uses regex to go through a list of specified characters
+    /// and replace them with empty spaces.
+    ///
+    /// 7. This method also uses two loops.  One to add or update from the string array, the other to get the values
+    /// into another dictionary, in a specified order.  You might be able to order the concurrent dictionary in the
+    /// return method.
+    /// 
+    /// 8. Changed the current method to not add an empty string to the dictionary.
+    /// </summary>
+    public static class Problem2
+    {
+        public static Dictionary<string, int> Process(string phrase)
+        {
+            string[] words =
+                phrase.Replace("\"", string.Empty)
+                    .Replace("'", string.Empty)
+                    .Replace(".", string.Empty)
+                    .Replace(",", string.Empty)
+                    .Replace("\n", string.Empty)
+                    .Replace("!", string.Empty)
+                    .Replace("?", string.Empty)
+                    .Split(' ');
+
+            var wordCount = new ConcurrentDictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                wordCount.AddOrUpdate(word, 1, (key, existingValue) =>
+                {
+                    if (word == key)
+                    {
+                        existingValue++;
+                    }
+
+                    return existingValue;
+                });
+            }
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            foreach (var pair in wordCount.OrderByDescending((s) => s.Key))
+            {
+                if (!string.IsNullOrEmpty(pair.Key))
+                {
+                    dictionary.Add(pair.Key, pair.Value);
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ paragraph-processor/InterviewProblemsTests/Problem2Tests.cs
@@ -0,0 +1,116 @@
+﻿using FluentAssertions;
+using InterviewProblems;
+using System.Collections.Generic;
+using Xunit;
+
+namespace InterviewProblemsTests
+{
+    public class Problem2Tests
+    {
+        /// <summary>
+        /// A passing test with the changes made to the existing version.
+        /// The original version had problems with the dictionary.
+        /// </summary>
+        [Fact]
+        public void given_short_phrase_when_processed_returns_expected_dictionary()
+        {
+            // Arrange.
+            const string phrase = @"Hello world! Oh dear! What has the world been up to?";
+            var expectedDictionary = new Dictionary<string, int>
+            {
+                {"Hello", 1},
+                {"world", 2},
+                {"Oh", 1},
+                {"dear", 1},
+                {"What", 1},
+                {"has", 1},
+                {"the", 1},
+                {"been", 1},
+                {"up", 1},
+                {"to", 1}
+            };
+
+            // Act.
+            var result = Problem2.Process(phrase);
+
+            // Assert.
+            result.ShouldBeEquivalentTo(expectedDictionary);
+        }
+
+        /// <summary>
+        /// A passing test, made possible with a small change to the method.  We don't want to have an
+        /// entry for an empty string.
+        /// </summary>
+        [Fact]
+        public void given_phrase_with_punctuation_when_processed_returns_empty_dictionary()
+        {
+            // Arrange.
+            const string phrase = "\"'.,!?";
+
+            // Act.
+            var result = Problem2.Process(phrase);
+
+            // Assert.
+            result.Count.Should().Be(0);
+        }
+
+        /// <summary>
+        /// A simple test to ensure we get the correct count of the same word.
+        /// </summary>
+        [Fact]
+        public void given_phrase_with_same_word_returns_one_entry()
+        {
+            // Arrange.
+            const string phrase = "hello hello hello hello";
+
+            // Act.
+            var result = Problem2.Process(phrase);
+
+            // Assert.
+            result.Count.Should().Be(1);
+        }
+
+        /// <summary>
+        /// A simple test to ensure we get the expected dictionary.
+        /// </summary>
+        [Fact]
+        public void given_phrase_with_same_word_returns_correct_dictionary()
+        {
+            // Arrange.
+            const string phrase = "hello hello hello hello";
+            var expectedDictionary = new Dictionary<string, int>
+            {
+                {"hello", 4}
+            };
+
+            // Act.
+            var result = Problem2.Process(phrase);
+
+            // Assert.
+            result.ShouldBeEquivalentTo(expectedDictionary);
+        }
+
+        /// <summary>
+        /// A simple test to ensure we get the expected dictionary depending on the case of the word.
+        /// We are ensuring that the case is considered a different word.
+        /// </summary>
+        [Fact]
+        public void given_phrase_with_same_word_but_with_different_case_returns_correct_dictionary()
+        {
+            // Arrange.
+            const string phrase = "hello Hello hello hello";
+            var expectedDictionary = new Dictionary<string, int>
+            {
+                {"hello", 3},
+                {"Hello", 1}
+            };
+
+            // Act.
+            var result = Problem2.Process(phrase);
+
+            // Assert.
+            result.ShouldBeEquivalentTo(expectedDictionary);
+        }
+
+    }
+}
